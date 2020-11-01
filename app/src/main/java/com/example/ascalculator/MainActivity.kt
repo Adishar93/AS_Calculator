782513c4242cc19411da55f45ba8508f22ab3ccb
aditya <adishar93@gmail.com>
2020-11-01 20:56:30 +0530

    Added textview and number pad along with backspace

--- app/src/main/java/com/example/ascalculator/MainActivity.kt
+++ app/src/main/java/com/example/ascalculator/MainActivity.kt
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.ascalculator.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
@@ -16,22 +18,64 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {sendMessage()}
+        val calScreen=findViewById<TextView>(R.id.calcScreen)
+
+        val button7 = findViewById<Button>(R.id.bSeven)
+        button7.setOnClickListener {updateCalculator("7")}
+        val button8 = findViewById<Button>(R.id.bEight)
+        button8.setOnClickListener {updateCalculator("8")}
+        val button9 = findViewById<Button>(R.id.bNine)
+        button9.setOnClickListener {updateCalculator("9")}
+        val button4 = findViewById<Button>(R.id.bFour)
+        button4.setOnClickListener {updateCalculator("4")}
+        val button5 = findViewById<Button>(R.id.bFive)
+        button5.setOnClickListener {updateCalculator("5")}
+        val button6 = findViewById<Button>(R.id.bSix)
+        button6.setOnClickListener {updateCalculator("6")}
+        val button1 = findViewById<Button>(R.id.bOne)
+        button1.setOnClickListener {updateCalculator("1")}
+        val button2 = findViewById<Button>(R.id.bTwo)
+        button2.setOnClickListener {updateCalculator("2")}
+        val button3 = findViewById<Button>(R.id.bThree)
+        button3.setOnClickListener {updateCalculator("3")}
+        val button0 = findViewById<Button>(R.id.bZero)
+        button0.setOnClickListener {updateCalculator("0")}
+
+        val buttonBackspace = findViewById<Button>(R.id.bBackspace)
+        buttonBackspace.setOnClickListener {updateCalculator("X")}
+
+
     }
 
     override fun onResume() {
         super.onResume()
-        Toast.makeText(
-            applicationContext, "This is fun!",
-            Toast.LENGTH_SHORT).show()
+//        Toast.makeText(
+//            applicationContext, "This is fun!",
+//            Toast.LENGTH_SHORT).show()
     }
 
-    private fun sendMessage() {
-        val message = "Hi!"
-        val intent = Intent(this, SecondActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
+    private fun updateCalculator(input:String) {
+
+        //BackSpace Button is Pressed
+        if(input=="X")
+        {
+            if(calcScreen.text.length==1&&calcScreen.text.toString()!="0")
+            {
+                calcScreen.text="0"
+            }
+            else if(calcScreen.text.toString()!="0")
+            {
+                calcScreen.text=calcScreen.text.toString().subSequence(0,calcScreen.text.length-1)
+            }
         }
-        startActivity(intent)
+        // Number on Keypad is pressed
+        else {
+            if (calcScreen.text.toString() == "0") {
+                calcScreen.text = input
+            } else {
+                calcScreen.text = calcScreen.text.toString() + input
+            }
+        }
+
     }
 }
